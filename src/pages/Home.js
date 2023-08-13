98e780ebf2f0af4dcdd710e4c652636c4c89aec6
Jagrut Sharma <101259156+jagrut-sharma@users.noreply.github.com>
2023-08-13 10:33:18 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useData } from "../context/dataContext";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="font-Merriweather text-3xl font-bold">Heading</h1>
+      <p className="font-Libre text-xl">Paragraph</p>
+    </>
+  );
+}
